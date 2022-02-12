9909141f8b7d7a980049b1b38ef770f204087d52
mosheurpau <mosheur.pau@gamil.com>
2022-02-12 13:02:09 +0600

    withdraw handeling

--- js/banking.js
+++ js/banking.js
@@ -11,6 +11,35 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
 
     depositTotal.innerText = depositTotalAmount + newDepositAmount;
 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = newDepositAmount + balanceTotalAmount;
+
 
     // clear input fild
-})
+    depositInput.value = '';
+});
+
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const withdrawInputAmount = parseFloat(withdrawInputText);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(withdrawTotalText);
+
+    withdrawTotal.innerText = withdrawTotalAmount + withdrawInputAmount;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = balanceTotalAmount - withdrawInputAmount;
+
+
+    // clear input filed 
+    withdrawInput.value = '';
+});
