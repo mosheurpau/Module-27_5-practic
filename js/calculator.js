5b356b5bc01d741e2c0ddf246b00d1e2ca91eac8
mosheurpau <mosheur.pau@gamil.com>
2022-02-12 22:41:07 +0600

    added calculator

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+function calculate() {
+    document.calc.output.value = eval(document.calc.output.value);
+    console.log(document.calc.output.value);
+}
+
+function erase() {
+    document.calc.output.value = '';
+}
+
+// variable s contains the actual value of the button pressed 
+function entries(s) {
+    document.calc.output.value += s;
+    console.log(s);
+}
